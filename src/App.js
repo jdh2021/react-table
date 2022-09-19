6121560a50f6731cf83636d80d7fbe125ad2b2f9
Jennifer Hightower <jenniferhightower2020@gmail.com>
2022-09-19 14:33:17 -0500

    added function for submitting form and adding a new record to table.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import data from './mock-data.json';
 import { useState } from 'react';
+import { nanoid } from 'nanoid'; // generates id for contact record
 
 const App = () => {
   const [contacts, setContacts] = useState(data);
-
   // store form values in state as object, update value as user types
   const [addFormData, setAddFormData] = useState({
     fullName: '',
@@ -13,6 +13,7 @@ const App = () => {
     email: '',
   })
 
+  // easier to add new values to form
   const handleAddFormChange = (event) => {
     console.log('in handleAddFormChange');
     event.preventDefault();
@@ -22,6 +23,23 @@ const App = () => {
     newFormData[fieldName] = fieldValue; // update newFormData object. fieldName is key. fieldValue is property.
     setAddFormData(newFormData);
   }
+
+  // called when form is submitted
+  const handleAddFormSubmit = (event) => {
+    // prevents form from doing POST request when submitted
+    event.preventDefault();
+    // take data and create new object
+    const newContact = {
+      id: nanoid(),
+      fullName: addFormData.fullName,
+      address: addFormData.address,
+      phoneNumber: addFormData.phoneNumber,
+      email: addFormData.email,
+    };
+    const newContacts = [...contacts, newContact];
+    setContacts(newContacts);
+  }
+
   return (<div className="app-container">
     <table>
       <thead>
@@ -44,7 +62,7 @@ const App = () => {
       </tbody>
     </table>
     <h2>Add A Contact</h2>
-    <form>
+    <form onSubmit={handleAddFormSubmit}>
       <input type="text" name="fullName" required="required" placeholder="Enter a name..." onChange={handleAddFormChange}></input>
       <input type="text" name="address" required="required" placeholder="Enter an address..." onChange={handleAddFormChange}></input>
       <input type="text" name="phoneNumber" required="required" placeholder="Enter a phone number..." onChange={handleAddFormChange}></input>
